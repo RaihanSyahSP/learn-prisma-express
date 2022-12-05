3c9c1d932d8700953294b000c32d29cf07d3c296
RaihanSyahSP <raihansyah45@gmail.com>
2022-12-05 09:32:24 +0700

    first commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import router from "./router";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("Hello from express");
+  res.status(200);
+  res.json({ message: "Hello World!" });
+});
+
+app.use("/api", router);
+
+export default app;
